9e8b5f1ce89bdfb6c1b9abf5cecb33a9310c7ef3
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-12-09 23:23:14 +0200

    telegram notifications add to views.py

--- borrowings/views.py
+++ borrowings/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+
+from notifications.utils import send_telegram_message
 from .models import Borrowing
 from .serializers import BorrowingSerializer
 
@@ -15,3 +17,7 @@ class BorrowingViewSet(ModelViewSet):
         if self.request.user.is_staff:
             return super().get_queryset()
         return Borrowing.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        borrowing = serializer.save(user=self.request.user)
+        send_telegram_message(f"New borrowing created: {borrowing.book.title}")
