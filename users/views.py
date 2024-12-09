5f3797abe1f5e54e077d92b514f55e285c5ab4fa
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-12-09 22:24:47 +0200

    views

---
+++ users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
