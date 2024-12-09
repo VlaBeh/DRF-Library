26030f5f96818e84baa8ed875864c70f51ba2804
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-12-09 22:15:49 +0200

    redo urls

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,13 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
+
+app_name = 'book'
 
 router = DefaultRouter()
 router.register('books', BookViewSet)
 urlpatterns = router.urls
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
